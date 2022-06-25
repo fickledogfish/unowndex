921297c94d9a00f7eb83e7b115babb2c6a140d44
fickledogfish <88333972+fickledogfish@users.noreply.github.com>
2022-06-25 16:25:27 -0300

    Exit the swiftlint script on failure if no path is provided

--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-declare -r pathToBeLinted=$1
+# Run the swiftlint tool on a specific path, which is expected to correspond to
+# a layer of the app.
+#
+# Dependencies:
+#	- swiftlint >= 0.47.1
+#
+# Changelog:
+#	[2022-06-25] Created script.
 
-if test -d "/opt/homebrew/bin/"; then
+declare -r pathToBeLinted=$1
+if test -z "$pathToBeLinted"; then
+	echo 'Script called without a path to pass to the linter'
+	exit 1
+fi
+
+if test -d '/opt/homebrew/bin/'; then
 	PATH="/opt/homebrew/bin/:${PATH}"
 fi
 
-export PATH
-
 if which swiftlint >/dev/null; then
-	swiftlint $pathToBeLinted
+	swiftlint "$pathToBeLinted"
 else
-	echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+	echo 'warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint'
 fi
