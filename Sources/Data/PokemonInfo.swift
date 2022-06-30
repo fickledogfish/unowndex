2719d1de9722e9112349e12cce4e07cc805972ed
fickledogfish <88333972+fickledogfish@users.noreply.github.com>
2022-06-30 11:05:08 -0300

    Add types to PokemonInfo

--- Sources/Data/PokemonInfo.swift
+++ Sources/Data/PokemonInfo.swift
@@ -1,9 +1,17 @@
 public struct PokemonInfo {
     public let id: Int
+    public let types: Types
 
     public init(
-        id: Int
+        id: Int,
+        types: Types
     ) {
         self.id = id
+        self.types = types
+    }
+
+    public enum Types {
+        case single(PokemonType)
+        case double(PokemonType, PokemonType)
     }
 }
--- Sources/Service/Extensions/PokemonInfo+InitDto.swift
+++ Sources/Service/Extensions/PokemonInfo+InitDto.swift
@@ -2,8 +2,24 @@ import Data
 
 internal extension PokemonInfo {
     init(dto: PokemonInfoDto) {
+        let types: PokemonInfo.Types
+        switch dto.types.count {
+        case 1:
+            types = .single(dto.types[0].type.name)
+
+        case 2:
+            types = .double(
+                dto.types[0].type.name,
+                dto.types[1].type.name
+            )
+
+        default:
+            fatalError()
+        }
+
         self.init(
-            id: dto.id
+            id: dto.id,
+            types: types
         )
     }
 }
