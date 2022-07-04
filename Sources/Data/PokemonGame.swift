5f04ce3e788c6987ed1155a1ae1f3a1fa15721e0
fickledogfish <88333972+fickledogfish@users.noreply.github.com>
2022-07-04 16:38:08 -0300

    Add all pokemon titles supported by the API in PokemonGame.swift

--- Sources/Data/PokemonGame.swift
+++ Sources/Data/PokemonGame.swift
@@ -1,3 +1,4 @@
+// swiftlint:disable identifier_name
 public enum PokemonGame: String, CaseIterable {
     case red, blue, green, yellow
 
@@ -12,10 +13,20 @@ public enum PokemonGame: String, CaseIterable {
     case black, white
     case black2 = "black-2", white2 = "white-2"
 
-    // swiftlint:disable identifier_name
+    case colosseum, xd
+
     case x, y
-    // swiftlint:enable identifier_name
     case omegaRuby = "omega-ruby", alphaSaphire = "alpha-saphire"
+
+    case sun, moon
+    case ultraSun = "ultra-sun", ultraMoon = "ultra-moon"
+
+    case letsGoPikachu = "lets-go-pikachu", letsGoEevee = "lets-go-eevee"
+
+    case sword, shield
+    case theIsleOfArmor = "the-isle-of-armor", theCrownTundra = "the-crown-tundra"
+    case brilliantDiamond = "brilliant-diamond", shiningPearl = "shining-pearl"
 
     case legendsArceus = "legends-arceus"
 }
+// swiftlint:enable identifier_name
